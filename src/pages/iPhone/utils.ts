9d929543bbe1fa59dc526b33e9ece9492128c698
Weijing Jay Lin <weijing_lin@intuit.com>
2023-11-02 00:11:54 -0700

    handle discount on the price

--- src/pages/iPhone/utils.ts
+++ src/pages/iPhone/utils.ts
@@ -3,7 +3,7 @@ export const regularRate = 1.1;
 
 export function getFairPrice(price: number, profitRate: number) {
   const ifDiscount = profitRate === discountRate ? true : false;
-  const margin = 200;
+  const margin = ifDiscount ? 150 : 200;
   return Number(
     price * profitRate - price > margin
       ? Number(price) + margin
