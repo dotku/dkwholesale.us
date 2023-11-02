f73c1f6c985793155cc120f3198d1c98c3bfbfd8
Weijing Jay Lin <weijing_lin@intuit.com>
2023-11-01 23:47:17 -0700

    add coupon

--- src/pages/iPhone/IPhone.js
+++ src/pages/iPhone/IPhone.js
@@ -5,6 +5,8 @@ export default function IPhone() {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null); // [error, setError
   const [data, setData] = useState([]);
+  const [profitRate, setProfitRate] = useState(1.1);
+  const [coupon, setCoupon] = useState("");
 
   useEffect(() => {
     setTimeout(() => {
@@ -26,6 +28,14 @@ export default function IPhone() {
     }, 1000);
   }, []);
 
+  const handleCouponInputSubmit = (e) => {
+    console.log("handleCouponInputSubmit");
+    if (["dk", "zhao", "kobe"].includes(coupon.toLocaleLowerCase())) {
+      alert("恭喜你，获得 5% 优惠券");
+      setProfitRate(1.05);
+    }
+  };
+
   return (
     <div>
       <h1>iPhone</h1>
@@ -38,28 +48,49 @@ export default function IPhone() {
       )}
       {error && <div className="alert alert-danger">{error}</div>}
       {!isLoading && !error && data && (
-        <table className="table">
-          <>
-            <thead>
-              <tr>
-                <th tile="Name">产品</th>
-                <th tile="Generation">代数</th>
-                <th tile="Storage">容量</th>
-                <th tile="Price">价格</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map(({ name, generation, storage, price }, idx) => (
-                <tr key={idx}>
-                  <td>{name}</td>
-                  <td>{generation}</td>
-                  <td>{storage}</td>
-                  <td>{(price * 1.1).toFixed(0)}</td>
+        <>
+          <table className="table">
+            <>
+              <thead>
+                <tr>
+                  <th tile="Name">产品</th>
+                  <th tile="Generation">代数</th>
+                  <th tile="Storage">容量</th>
+                  <th tile="Price">价格</th>
                 </tr>
-              ))}
-            </tbody>
-          </>
-        </table>
+              </thead>
+              <tbody>
+                {data.map(({ name, generation, storage, price }, idx) => (
+                  <tr key={idx}>
+                    <td>{name}</td>
+                    <td>{generation}</td>
+                    <td>{storage}</td>
+                    <td>{(price * profitRate).toFixed(0)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </>
+          </table>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Coupon 优惠券"
+              aria-label="Coupon 优惠券"
+              aria-describedby="button-addon2"
+              value={coupon}
+              onChange={(e) => setCoupon(e.target.value)}
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
+              onClick={handleCouponInputSubmit}
+            >
+              提交
+            </button>
+          </div>
+        </>
       )}
     </div>
   );
