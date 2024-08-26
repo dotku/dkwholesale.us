01298e9571f43d17730fdabc8ce2d5421949c3c8
jay-itl <weijingjaylin@gmail.com>
2024-08-26 03:27:02 -0700

    update footer current year

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,8 @@ export default function Footer() {
     <div id="footer" className="my-5 text-center">
       <ContactUs />
       <div className="small text-align-center">
-        &copy;2023 DKWholesale (US), tehcnologies provide by{" "}
+        &copy;2023 - {new Date().getFullYear()} DKWholesale (US), tehcnologies
+        provide by{" "}
         <a href="http://jytech.us" style={{ color: "inherit" }}>
           JYTech
         </a>{" "}
