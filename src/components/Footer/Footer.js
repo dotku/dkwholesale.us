f7f3fad8465cf4ec4fd0f5683b6b6ecbfac80466
jay-itl <weijingjaylin@gmail.com>
2024-08-26 03:28:51 -0700

    update copyright wording on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,11 @@ export default function Footer() {
     <div id="footer" className="my-5 text-center">
       <ContactUs />
       <div className="small text-align-center">
-        &copy;2023 - {new Date().getFullYear()} DKWholesale (US), tehcnologies
-        provide by{" "}
+        &copy;2023 - {new Date().getFullYear()} DKWholesale LLC, partnered with
+        tehcnologies service provider{" "}
         <a href="http://jytech.us" style={{ color: "inherit" }}>
           JYTech
-        </a>{" "}
-        LLC
+        </a>
       </div>
     </div>
   );
