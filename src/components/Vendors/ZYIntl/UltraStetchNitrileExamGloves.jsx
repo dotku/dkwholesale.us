87ee1ecf50c723270071b2e3ce666fd0dbef21b4
jay-itl <weijingjaylin@gmail.com>
2024-08-26 03:22:49 -0700

    add key on image list

--- src/components/Vendors/ZYIntl/UltraStetchNitrileExamGloves.jsx
+++ src/components/Vendors/ZYIntl/UltraStetchNitrileExamGloves.jsx
@@ -15,6 +15,7 @@ export default function UltraStretchNitrileExamGloves() {
       <div className="row">
         {productImages.map((image) => (
           <img
+            key={image}
             src={`/assets/images/ultra-strech-nitrile-exam-gloves/${image}`}
             alt="Ultra Stretch Nitrile Exam Gloves"
             className="rounded mb-3 col-12 col-sm-6"
