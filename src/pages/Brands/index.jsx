eaa809aab48e859128f04296f5d4234980a7f13c
jay-itl <weijingjaylin@gmail.com>
2024-08-26 01:57:20 -0700

    add qiutai, carku betavee and more

--- src/pages/Brands/index.jsx
+++ src/pages/Brands/index.jsx
@@ -3,6 +3,15 @@ export default function Brands() {
     <>
       <ul>
         <li>
+          <a href="https://qiutaisweater.en.alibaba.com/">Qiutai Sweater</a>
+        </li>
+        <li>
+          <a href="http://car-ku.com">Carku</a>
+        </li>
+        <li>
+          <a href="http://betavee.com">Betavee</a>
+        </li>
+        <li>
           <a href="http://jp-kagado.com">Kogado Perfume</a>
         </li>
       </ul>
