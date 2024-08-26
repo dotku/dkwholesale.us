25ecdb7e11724700de039cb1009200f5df2456d4
jay-itl <weijingjaylin@gmail.com>
2024-08-26 02:08:10 -0700

    update the brand pages

--- src/pages/Brands/index.jsx
+++ src/pages/Brands/index.jsx
@@ -3,22 +3,34 @@ export default function Brands() {
     <>
       <ul>
         <li>
-          <a href="https://zyintltrade.com/">ZY International Trade</a>
+          <a href="https://zyintltrade.com/">ZY International Trade</a> Nitrile
+          Gloves
         </li>
         <li>
-          <a href="https://usproglove.com/">Ultra Strech</a>
+          <a href="https://usproglove.com/">Ultra Strech</a> Nitrile Gloves
         </li>
         <li>
-          <a href="https://qiutaisweater.en.alibaba.com/">Qiutai Sweater</a>
+          <a href="https://qiutaisweater.en.alibaba.com/">Qiutai</a> Sweater
         </li>
         <li>
-          <a href="http://car-ku.com">Carku</a>
+          <a href="https://herotech.dkwholesale.us">Haroctech</a> 3C digital
+          products, iPhones
         </li>
         <li>
-          <a href="http://betavee.com">Betavee</a>
+          <a href="http://car-ku.com">Carku</a> Jump start
         </li>
         <li>
-          <a href="http://jp-kagado.com">Kogado Perfume</a>
+          <a href="http://betavee.com">Betavee</a> Battery Bank
+        </li>
+        <li>
+          <a href="http://jp-kagado.com">Kogado</a> Perfume
+        </li>
+        <li>
+          <a href="https://dotku.us/dongguan-houjie-yusheng-shoes/">Yusheng</a>{" "}
+          Shoes
+        </li>
+        <li>
+          <a href="https://ouxi.us">Ouxi</a> eBike
         </li>
       </ul>
     </>
