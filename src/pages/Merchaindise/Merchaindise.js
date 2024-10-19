53e14378993c8afb9b5a53c5c61b709ec9eb1d44
Weijing Jay Lin <weijingjaylin@gmail.com>
2024-10-18 21:21:31 -0700

    remove kolb shop fund

--- src/pages/Merchaindise/Merchaindise.js
+++ src/pages/Merchaindise/Merchaindise.js
@@ -7,31 +7,6 @@ export default function Merchaindise() {
       <div className="h2">日用商品</div>
       <MagonioProduct />
       <UltraStretchNitrileExamGloves />
-      <center>
-        <img
-          src="https://github.com/dotku/dkwholesale.us/assets/1519232/7f765a92-75dc-4f72-903e-62574d8e3b5c"
-          alt="Merchaindise"
-          className="img-fluid rounded"
-        />
-      </center>
-      <p>
-        My group, Kolb Elementary School, kicked off our fundraiser and I need
-        your help! We are raising $25,000 for classroom supplies and grants,
-        library grants, community events and more.
-      </p>
-      <p>
-        Will you please help me reach my goal? You can support my fundraiser by
-        going to the link below. If you don&apos;t use my personal shopping
-        link, you need to go to https://www.ShopFund.com click SHOP NOW and then
-        put in my Student Code: NFR18568.
-      </p>
-      <p>
-        My Personal Shopping link is:{" "}
-        <a href="https://www.shopfund.com/Shop?sid=2100584&t=EM">
-          https://www.shopfund.com/Shop?sid=2100584&t=EM
-        </a>
-      </p>
-      <p>Thank you so much and let me know when you are done :)</p>
     </>
   );
 }
